7c68ddae51f8fcb8d5cf2b20b07a87c44b02f411
suryavip <suryabejibun@gmail.com>
2023-02-24 22:45:44 +0700

    copy main

--- 99.main.ino
+++ 99.main.ino
@@ -1,11 +1,23 @@
+bool setupDone = false;
+
 void setup() {
-  
+  debugLedStart();
+
+  comSetup();
+  sensorSetup();
+//  motorSetup();
+
+  debugLedStop();
+
+  setupDone = true;
 }
 
 void loop() {
-  
+  sensor.update();
+  comLoop();
 }
 
 void loop1() {
-  
+  if (setupDone == false) return;
+  motorLoop();
 }
