3cc336d71399cf02b71b2701bf06a5de2eae1bfe
suryavip <suryabejibun@gmail.com>
2023-07-15 14:55:53 +0700

    adjust zero_electric_angle in realtime to achieve higher velocity and refactor easier mappings

--- 05.motor.ino
+++ 05.motor.ino
@@ -1,6 +1,7 @@
 const unsigned int MOTOR_POLE_PAIRS = 15;
 const unsigned int MOTOR_VOLTAGE_LIMIT = 10;
 const unsigned int MOTOR_VOLTAGE_LIMIT_FOR_ALIGNMENT = 4;
+const float MOTOR_ZERO_ELECTRICAL_ANGLE = 2.79; // DON'T FORGOT TO ADJUST THIS
 
 BLDCMotor motor = BLDCMotor(MOTOR_POLE_PAIRS);
 Kalman veloFilter(0.01, 32, 1023, 0);
@@ -18,58 +19,44 @@ void keepTrackVelo() {
   float velo = sensor.getVelocity();
   filteredVelo = veloFilter.getFilteredValue(velo);
 
-  if (isMotorDebug) {
-    Serial.print("VELO:");
-    Serial.print(filteredVelo);
-    Serial.println(";");
-  }
-}
-
-int veloReactiveMagnitude() {
-  int magnitude = savedVeloReactiveMagnitude;
-
-  if (filteredVelo > .3) magnitude *= 1;
-  else if (filteredVelo < -.3) magnitude *= -1;
-  else magnitude = 0;
-
-  return magnitude;
+  if (isMotorDebug) Serial.println(filteredVelo);
 }
 
 float additionalVoltageMultiplier() {
-  int absVelo = abs(filteredVelo);
+  float mapIn[]   = { -31, -30, -.5, .5, 30, 31};
+  float mapOut[]  = {   2,   2,   1,  1,  2,  2};
+  float mapResult = multiMap<float>(
+                      filteredVelo,
+                      mapIn,
+                      mapOut,
+                      6);
 
-  if (absVelo > 30) absVelo = 30;
+  return mapResult;
+}
 
-  float mapVeloIn[] = {0, .5, 30};
-  float mapMultOut[] = {1, 1, 2};
-  float mappedMult = multiMap<float>(
-                       absVelo,
-                       mapVeloIn,
-                       mapMultOut,
-                       3);
+float calculateZeroElectricAngle() {
+  float mapIn[]   = { -31, -30, -.5, .5,  30,  31};
+  float mapOut[]  = {  .2,  .2,   0,  0, -.2, -.2};
+  float mapResult = multiMap<float>(
+                      filteredVelo,
+                      mapIn,
+                      mapOut,
+                      6);
 
-  if (isMotorDebug) Serial.println(mappedMult);
-  return mappedMult;
+  return MOTOR_ZERO_ELECTRICAL_ANGLE + mapResult;
 }
 
 float magnitudeToTarget(int magnitude) {
-  int absMagnitude = abs(magnitude);
+  float mapIn[] =  { -10001, -10000,  -1, 0,  1, 10000, 10001};
+  float mapOut[] = {     -7,     -7, -.9, 0, .9,     7,     7};
+  float mapResult = multiMap<float>(
+                      magnitude,
+                      mapIn,
+                      mapOut,
+                      7);
 
-  if (absMagnitude > 10000) absMagnitude = 10000;
-
-  float mapMagnitudeIn[] = {0, 1, 10000};
-  float mapTargetOut[] = {0, .9, 7};
-  float mappedTarget = multiMap<float>(
-                         absMagnitude,
-                         mapMagnitudeIn,
-                         mapTargetOut,
-                         3);
-
-  // Apply back direction.
-  if (magnitude < 0) mappedTarget *= -1;
-
-  if (isMotorDebug) Serial.println(mappedTarget);
-  return mappedTarget;
+  if (isMotorDebug) Serial.println(mapResult);
+  return mapResult;
 }
 
 void motorSetup() {
@@ -84,7 +71,7 @@ void motorSetup() {
   motor.torque_controller = TorqueControlType::voltage;
   motor.foc_modulation = FOCModulationType::SinePWM;
   motor.motion_downsample = 2;
-  motor.zero_electric_angle = 2.80; // DON'T FORGOT TO ADJUST THIS
+  motor.zero_electric_angle = MOTOR_ZERO_ELECTRICAL_ANGLE;
 
   motor.init();
   motor.initFOC();
@@ -105,11 +92,10 @@ void motorLoop() {
     lastMotorRequestMagnitude = 0;
   }
 
-  keepTrackVelo();
-  int magnitude = lastMotorRequestMagnitude + veloReactiveMagnitude();
-
   // Normal FOC routine.
-  motor.target = magnitudeToTarget(magnitude) * additionalVoltageMultiplier();
+  keepTrackVelo();
+  motor.zero_electric_angle = calculateZeroElectricAngle();
+  motor.target = magnitudeToTarget(lastMotorRequestMagnitude) * additionalVoltageMultiplier();
   motor.loopFOC();
   motor.move();
 }
