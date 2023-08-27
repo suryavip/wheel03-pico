2578cf3c164b5b2356dd7acc2ee7a1ed70ec1e79
suryavip <suryabejibun@gmail.com>
2023-08-27 21:35:38 +0700

    remove kalman filter library (no longer needed)

--- wheel03-pico.ino
+++ wheel03-pico.ino
@@ -1,7 +1,6 @@
 #include <SimpleFOC.h> // https://github.com/simplefoc
 #include <AS5600.h>    // https://github.com/RobTillaart/AS5600
 #include <MultiMap.h>  // https://github.com/RobTillaart/MultiMap
-#include <Kalman.h>    // https://github.com/bachagas/Kalman
 #include <LittleFS.h>
 #include <ADS1X15.h>   // https://github.com/RobTillaart/ADS1X15
 
