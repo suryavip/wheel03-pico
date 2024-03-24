8e1529cb220d84d9da5fab41b1cdb872c48f6b36
Sudhanta Suryaputra <suryabejibun@gmail.com>
2024-03-24 16:13:22 +0700

    add whole rotation ZEA detection

--- 05.motor.ino
+++ 05.motor.ino
@@ -1,13 +1,14 @@
 const unsigned int MOTOR_POLE_PAIRS = 15;
 const unsigned int MOTOR_VOLTAGE_LIMIT = 9;
-const unsigned int MOTOR_VOLTAGE_LIMIT_FOR_ALIGNMENT = 4;
-//const float MOTOR_ZERO_ELECTRICAL_ANGLE = 2.80; // DON'T FORGOT TO ADJUST THIS
+const unsigned int MOTOR_VOLTAGE_LIMIT_FOR_ALIGNMENT = 2;
 
 BLDCMotor motor = BLDCMotor(MOTOR_POLE_PAIRS);
 
 unsigned int lastMotorRequestMillis = 0;
 float lastMotorRequestVoltage = 0;
-float zeroElectricalAngle;
+
+float zeaPositions[MOTOR_POLE_PAIRS];
+float zeas[MOTOR_POLE_PAIRS];
 
 void setRequestVoltage(float v) {
   lastMotorRequestVoltage = v;
@@ -33,6 +34,15 @@ float zeaOffsetByVelo() {
   return mapResult;
 }
 
+float zeaByPosition() {
+  float mapResult = multiMap<float>(
+    float(currentRawAngle),
+    zeaPositions,
+    zeas,
+    MOTOR_POLE_PAIRS);
+  return mapResult;
+}
+
 void motorSetup() {
   motor.linkSensor(&sensor);
   motor.linkDriver(&driver);
@@ -45,12 +55,11 @@ void motorSetup() {
   motor.torque_controller = TorqueControlType::voltage;
   motor.foc_modulation = FOCModulationType::SinePWM;
   motor.motion_downsample = 2;
-  //  motor.zero_electric_angle = MOTOR_ZERO_ELECTRICAL_ANGLE;
 
   motor.init();
   motor.initFOC();
 
-  zeroElectricalAngle = motor.zero_electric_angle;
+  sensorLinearizer();
 }
 
 void motorLoop() {
@@ -69,9 +78,75 @@ void motorLoop() {
   }
 
   // Normal FOC routine.
-  motor.zero_electric_angle = zeroElectricalAngle + zeaOffsetByVelo();
+  motor.zero_electric_angle = zeaByPosition() + zeaOffsetByVelo();
   motor.target = lastMotorRequestVoltage * voltageMultiplierByVelo();
 
   motor.loopFOC();
   motor.move();
+}
+
+void sensorLinearizer() {
+  float elAngle = _3PI_2;
+  const int transition = 384;
+
+  int p[MOTOR_POLE_PAIRS];
+  float z[MOTOR_POLE_PAIRS];
+
+  for (int i = 0; i < MOTOR_POLE_PAIRS; i++) {
+    motor.setPhaseVoltage(MOTOR_VOLTAGE_LIMIT_FOR_ALIGNMENT, 0, elAngle);
+    _delay(700);
+
+    sensor.update();
+    motor.zero_electric_angle = 0;
+    float thisEa = motor.electricalAngle();
+    p[i] = currentRawAngle;
+    z[i] = thisEa + .23;
+
+    if (i == MOTOR_POLE_PAIRS - 1) break;
+
+    for (int j = 0; j < transition; j++) {
+      elAngle = elAngle + (_2PI / transition);
+      motor.setPhaseVoltage(MOTOR_VOLTAGE_LIMIT_FOR_ALIGNMENT, 0, elAngle);
+      sensor.update();
+      _delay(1);
+    }
+  }
+
+  overRotation = 0;
+
+  // find the starting index
+  int l = SENSOR_PPR;
+  int li = 0;
+  for (int i = 0; i < MOTOR_POLE_PAIRS; i++) {
+    if (p[i] < l) {
+      l = p[i];
+      li = i;
+    }
+  }
+
+  // put sorted to global var
+  for (int i = li; i < MOTOR_POLE_PAIRS; i++) {
+    zeaPositions[i - li] = float(p[i]);
+    zeas[i - li] = z[i];
+  }
+  for (int i = 0; i < li; i++) {
+    zeaPositions[i + MOTOR_POLE_PAIRS - li] = float(p[i]);
+    zeas[i + MOTOR_POLE_PAIRS - li] = z[i];
+  }
+
+  if (isMotorDebug) {
+    for (int i = 0; i < MOTOR_POLE_PAIRS; i++) {
+      Serial.print(p[i]);
+      Serial.print(":");
+      Serial.println(z[i]);
+    }
+
+    Serial.println("---");
+
+    for (int i = 0; i < MOTOR_POLE_PAIRS; i++) {
+      Serial.print(zeaPositions[i]);
+      Serial.print(":");
+      Serial.println(zeas[i]);
+    }
+  }
 }
