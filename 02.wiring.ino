eb92d064b4e50ccab9cae7779345fc92c4632133
Sudhanta Suryaputra <suryabejibun@gmail.com>
2024-08-20 21:50:13 +0700

    change wiring for upcoming expansion PCB for pico

--- 02.wiring.ino
+++ 02.wiring.ino
@@ -1,12 +1,12 @@
 const pin_size_t PIN_SENSOR_SDA = 0;
 const pin_size_t PIN_SENSOR_SCL = 1;
 
-const pin_size_t PIN_PA_EN = 12;  // to R_EN (1st IBT-2 module)
-const pin_size_t PIN_PA = 13;     // to RPWM (1st IBT-2 module)
-const pin_size_t PIN_PB_EN = 14;  // to L_EN (1st IBT-2 module)
-const pin_size_t PIN_PB = 15;     // to LPWM (1st IBT-2 module)
-const pin_size_t PIN_PC_EN = 16;  // to R_EN (2nd IBT-2 module)
-const pin_size_t PIN_PC = 17;     // to RPWM (2nd IBT-2 module)
+const pin_size_t PIN_PA_EN = 16;  // to R_EN (1st IBT-2 module)
+const pin_size_t PIN_PA = 17;     // to RPWM (1st IBT-2 module)
+const pin_size_t PIN_PB_EN = 18;  // to L_EN (1st IBT-2 module)
+const pin_size_t PIN_PB = 19;     // to LPWM (1st IBT-2 module)
+const pin_size_t PIN_PC_EN = 20;  // to R_EN (2nd IBT-2 module)
+const pin_size_t PIN_PC = 21;     // to RPWM (2nd IBT-2 module)
 
 // Phase A will be on M+ terminal on 1st IBT-2 module.
 // Phase B will be on M- terminal on 1st IBT-2 module.
@@ -18,5 +18,5 @@ const pin_size_t PIN_ACCELERATOR = 0;  // on ADS1115
 const pin_size_t PIN_BRAKE = 1;        // on ADS1115
 const pin_size_t PIN_CLUTCH = 2;       // on ADS1115
 
-const pin_size_t PIN_LOADCELL_DT = 4;
-const pin_size_t PIN_LOADCELL_CL = 5;
+const pin_size_t PIN_LOADCELL_DT = 15;
+const pin_size_t PIN_LOADCELL_CL = 14;
