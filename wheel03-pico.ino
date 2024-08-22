340f0d484ffc7a3191fc160da6ec199f98199b6e
Sudhanta Suryaputra <suryabejibun@gmail.com>
2024-08-22 19:15:49 +0700

    update board and library versions

--- wheel03-pico.ino
+++ wheel03-pico.ino
@@ -1,10 +1,10 @@
-// https://github.com/earlephilhower/arduino-pico @3.9.3
+// https://github.com/earlephilhower/arduino-pico @3.9.5
 
 #include <SimpleFOC.h>  // https://github.com/simplefoc @2.3.4
 #include <AS5600.h>     // https://github.com/RobTillaart/AS5600 @0.6.1
 #include <MultiMap.h>   // https://github.com/RobTillaart/MultiMap @0.2.0
 #include <LittleFS.h>
-#include <ADS1X15.h>  // https://github.com/RobTillaart/ADS1X15 @0.4.5
+#include <ADS1X15.h>  // https://github.com/RobTillaart/ADS1X15 @0.5.0
 #include <HX711.h>    // https://github.com/bogde/HX711 @0.7.5
 
 #define isConfigDebug false
