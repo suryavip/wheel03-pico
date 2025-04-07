4ef13a36dbb7658c75ac8b10183ff582b9b2ad0a
Sudhanta Suryaputra <suryabejibun@gmail.com>
2025-04-07 23:28:20 +0700

    update libraries

--- wheel03-pico.ino
+++ wheel03-pico.ino
@@ -1,9 +1,9 @@
-// https://github.com/earlephilhower/arduino-pico @3.9.5
+// https://github.com/earlephilhower/arduino-pico @4.5.1
 
 #include <SimpleFOC.h>  // https://github.com/simplefoc @2.3.4
-#include <AS5600.h>     // https://github.com/RobTillaart/AS5600 @0.6.1
+#include <AS5600.h>     // https://github.com/RobTillaart/AS5600 @0.6.5
 #include <MultiMap.h>   // https://github.com/RobTillaart/MultiMap @0.2.0
 #include <LittleFS.h>
-#include <ADS1X15.h>   // https://github.com/RobTillaart/ADS1X15 @0.5.0
 #include <HX711.h>     // https://github.com/bogde/HX711 @0.7.5
+#include <ADS1X15.h>  // https://github.com/RobTillaart/ADS1X15 @0.5.2
 #include <KickSort.h>  // https://github.com/LinnesLab/KickSort @1.1.0
